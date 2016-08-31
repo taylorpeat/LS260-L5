99b8ec82b68ff50e7e764c0f369ae7ba131ec24a
Taylor Peat <taylorpeat@hotmail.com>
2016-08-31 15:26:33 -0400

    Add spec

--- music_library/specs/route_spec.js
+++ music_library/specs/route_spec.js
@@ -1,1 +1,26 @@
-var request = require("request");
+var request = require("request"),
+    root = "http://localhost:3000/";
+
+describe("JSON routes", function() {
+  var albums;
+
+  describe("/albums.json", function() {
+    it("check if json is returned", function(done) {
+      request(root + "albums.json", function(e, resp, body) {
+        albums = JSON.parse(body);
+        expect(albums[0].artist).toBeDefined();
+        done();
+      });
+    });
+  });
+
+  describe("/albums/<album>.json", function() {
+    it("returns an array of tracks", function(done) {
+      request(root + "albums/" + albums[0].title + ".json", function(e, resp, body) {
+        var tracks = JSON.parse(body);
+        expect(tracks[0].title).toBeDefined();
+        done();
+      });
+    });
+  });
+});
