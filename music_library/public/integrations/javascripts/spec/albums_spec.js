b538537ec479022a58e1844a86768045c25315f6
Taylor Peat <taylorpeat@hotmail.com>
2016-09-07 23:41:16 -0400

    finish view tests

--- music_library/public/integrations/javascripts/spec/albums_spec.js
+++ music_library/public/integrations/javascripts/spec/albums_spec.js
@@ -1,14 +1,38 @@
 describe("albums", function() {
-  it("Collection is created on init", function(done) {
-    var albumsLoaded = App.albumsLoaded;
+  beforeEach(function(done) {
     App.albumsLoaded = function(){
-      expect(App.albums.models.length).toBe(3);
-      expect(typeof App.albums.models[0].attributes.title).toBe("string");
       done();
     }
 
     App.init();
   });
 
-  it("")
+  it("Collection is created on init", function() {
+    expect(typeof App.albums.models[0].attributes.title).toBe("string");
+    expect(App.albums.models.length).toBe(3);
+  });
+
+  it("Has compiled handlebars template", function() {
+    expect(typeof App.view.template).toBe("function");
+  });
+
+  it("Renders to an #albums container", function() {
+    App.view.render();
+    expect($("#albums").children("li").length).toEqual(3);
+  });
+
+  it("Rerender view when albums change", function() {
+    var old_albums,
+        new_albums;
+    
+    old_albums = $("#albums").children("li");
+    App.albums.models[0].set("artist", "DaVinci");
+    new_albums = $("#albums").children("li");
+    expect(new_albums).not.toEqual(old_albums);
+  });
+
+  it("Verify tracks_url is set on model", function() {
+    var track_url = App.albums.models[0].get("track_url");
+    expect(track_url).toMatch(/album/);
+  });
 });
--- music_library/public/javascripts/models/album.js
+++ music_library/public/javascripts/models/album.js
@@ -1,6 +1,10 @@
 var Album = Backbone.Model.extend({
   parse: function(attrs) {
-    attrs.track_url = "/album/" + attrs.title;
+    attrs.track_url = "/album/" + Slugify(attrs.title);
     return attrs;
   }
 });
+
+function Slugify(title) {
+  return title.replace(/\s/g, "-").replace(/[^A-Za-z0-9\-]/g, "");
+}
